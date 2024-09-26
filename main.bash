9c223caa05f32df85ef539290afb1fdbd855cb2f
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-09-26 09:26:11 +0900

    [upd] fix

--- main.bash
+++ main.bash
@@ -30,13 +30,21 @@ send_message(){
     curl -X POST -H "Content-type: application/json" -d "${payload}" ${webhook_url}
 }
 
-send_message "start checking" logs
-list_instances | while read instance_id; do
-  if [[ ${instance_id} =  "i-0f488af9d15dd891c" ]]; then # データ収集用のインスタンス
-    echo "skip"
+check_instance(){
+  local instance_id=$1
+  if [[ ${instance_id} = "i-0f488af9d15dd891c" ]]; then # データ収集用のインスタンス
+    return 0;    
   elif [[ ${instance_id} = "i-09c7895d018cd7908" ]]; then # GMO_Data
-    echo "skip"
+    return 0;
   else
+    return 1;
+  fi  
+}
+
+send_message "start checking" logs
+for instance_id in $(list_instances); do
+  echo ${instance_id}
+  if ! check_instance ${instance_id}; then
     send_message "<@hotoku> ${instance_id} is running" warns
   fi
 done
